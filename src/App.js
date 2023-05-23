f598590ea3b5b0271f52113d20c306ac4d1819cf
sachindolase <sachindolase@gmail.com>
2023-05-23 21:46:51 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect , useState ,handleGenreChange , genre} from 'react';
 
-function App() {
+const movies = [
+  {
+    id: 1,
+    title: 'The Shawshank Redemption',
+    genre: 'Drama',
+    rating: 9.3
+  },
+  {
+    id: 2,
+    title: 'The Godfather',
+    genre: 'Crime',
+    rating: 9.2
+  },
+  {
+    id: 3,
+    title: 'Pulp Fiction',
+    genre: 'Crime',
+    rating: 8.9
+  },
+  
+];
+
+const MovieList = ({ movies }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {movies.map(movie => (
+        <li key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>Genre: {movie.genre}</p>
+          <p>Rating: {movie.rating}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+  const App = () => {
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      fetch('movies')
+        .then(response => response.json())
+        .then(data => setMovies(data))
+        .catch(error => console.log(error));
+    }, []);
+
+  const filteredMovies = genre ? movies.filter(movie => movie.genre === genre) : movies;
+
+
+
+
+  return (
+    <div>
+      <center>
+      <h1>Movie Recommendation App</h1>
+      <label htmlFor="genre">Select genre:</label>
+      <select id="genre" value={genre} onChange={handleGenreChange}>
+        <option value="">All</option>
+        <option value="Drama">Drama</option>
+        <option value="Crime">Crime</option>
+       </select>
+      <MovieList movies={filteredMovies} />
+      </center>
     </div>
   );
-}
+  };
 
 export default App;
